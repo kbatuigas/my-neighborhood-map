79b790aea7138260eb671c568a926266a821b1a0
kbatuigas <kbatuigas@gmail.com>
2019-01-22 00:32:13 -0800

    Files from local project

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { loadMap } from './utils';
+
 class App extends Component {
+ componentDidMount() {
+   //will be easier to access / interact with data as properties
+   let mapsPromise = loadMap();
+
+   Promise.all([
+     mapsPromise
+   ]).then(values => {
+     let google = values[0];
+     
+     this.google = google; 
+     this.map = new google.maps.Map(document.getElementById('map'), {
+       zoom: 13,
+       center: { lat: 45.5122, lng: -122.6587 }
+     });
+
+   });
+ }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="map">
       </div>
     );
   }
